e18a4c49d05490c0445a34145511e385e4bc8a44
Aashish518 <jadavaashish9@gmail.com.com>
2025-06-01 16:31:01 +0530

    ggg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
 })
