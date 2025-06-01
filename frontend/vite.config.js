88d5757dd61fee39cff2d52241f287d6294f891b
Aashish518 <jadavaashish9@gmail.com.com>
2025-06-01 16:33:37 +0530

    rrr

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
