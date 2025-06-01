234e6ad781961493a84065f02452ed9029c4eb87
Aashish518 <jadavaashish9@gmail.com.com>
2025-06-01 17:18:10 +0530

    ddd

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const mongodb = () => {
-    mongoose.connect(process.env.databaseurl)
+    mongoose.connect(process.env.databaseurl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
         .then(() => { console.log("Connection Successfully!") })
         .catch((err) =>{console.log("Not Connected")})
 }
